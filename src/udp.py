e6f847700a9ba423bdf5f27695acb301d785eeb6
Aran Leite <hyoretsu@gmail.com>
2024-10-25 21:17:22 -0300

    Ajuste para a interface

--- src/udp.py
+++ src/udp.py
@@ -6,6 +6,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 def send_message(message_kind: int) -> str:
+    # Associando o tipo da requisição vindo da interface aos tipos definidos na especificação
+    message_kind -= 1
+
     # Mesagens do tipo requisição são sempre 0
     message_type = 0
 
